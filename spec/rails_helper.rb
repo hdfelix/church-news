d46e08efcadf358c18fabc6411d06cfd2c0c1c28
Hector D. Felix <hdfelix@gmail.com>
2020-08-11 15:29:23 -0700

    Project setup:
    Initial configuration of:
    * RSpec
    * Database Cleaner
    * Shoulda matchers
    * Factory Bot
    * Simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,9 +30,13 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # Generate factories automatically for models generated on the command line
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -61,4 +65,27 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+# Database Cleaner config
+config.before(:suite) do
+  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.clean_with(:truncation)
+end
+
+config.before do
+  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner.start
+end
+
+config.append_after do
+  DatabaseCleaner.clean
+end
+
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
